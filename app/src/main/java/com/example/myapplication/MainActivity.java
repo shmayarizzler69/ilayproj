84adea0b7d173b6719185e91f174345d96c4f410
shmayarizzler69 <ilayshimony22@gmail.com>
2024-12-26 12:16:41 +0200

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.myapplication;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnRegister,btnLogin,btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        initViews();
+    }
+
+    private void initViews() {
+        btnRegister = findViewById(R.id.btnRegister);
+        btnRegister.setOnClickListener(this);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+        btnAbout = findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v==btnRegister){
+
+            Intent goReg=new Intent(getApplicationContext(), Register.class);
+            startActivity(goReg);
+        }
+        if (v==btnLogin){
+            Intent golog=new Intent(getApplicationContext(), Login.class);
+            startActivity(golog);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -0,0 +1,96 @@
+package com.example.myapplication.model;
+
+public class User {
+    String id;
+    String fname, lname, phone,email, password, city;
+
+    public User(String id, String fname, String lname, String phone, String email, String password, String city) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+        this.city = city;
+    }
+    public User(String id, String fname, String lname, String phone, String email, String city) {
+        this.id = id;
+        this.fname = fname;
+        this.lname = lname;
+        this.phone = phone;
+        this.email = email;
+        this.city = city;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
